10014c9511b45acf00f1fcd490939822dbaca9d4
bipulkuri <bkk9893@nyu.edu>
2023-02-03 20:46:36 -0500

    add phpmyadmin

--- start.sh
+++ start.sh
@@ -6,9 +6,18 @@ sleep 5
 # network view
 # docker network ls
 
-# SQL login
+# SQL login cli
 #docker ps
 #CONTAINER ID   IMAGE            COMMAND                  CREATED         STATUS         PORTS                    NAMES
 #14806720ff1b   mariadb:latest   "docker-entrypoint.s…"   46 seconds ago   Up 45 seconds   0.0.0.0:3306->3306/tcp   csgy6083-db-1
 #
-docker run -it --network db-network   --rm mariadb mariadb  -h mariadb-host -u root -p
+#docker run -it --network db-network   --rm mariadb mariadb  -h mariadb-host -u root -p
+
+#PHPmyadmin
+echo "open in browser" 
+echo "http://localhost:8080/index.php?route=/database/structure&db=my_guitar_shop"
+
+echo "server:mariadb-host"
+echo "user:root"
+echo "password:<<from docker-compose file>>"
+echo "END"
